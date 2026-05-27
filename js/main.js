19c1f034bd5c9533917c8eb78b9cbbd1b9a27186
cgy0603 <chlrmsdud0603@gmail.com>
2026-05-27 10:54:40 +0900

    aos적용 contact,skill 파트 추가

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const mobileMenu = document.querySelector(".mobile-menu");
 menuBtn.addEventListener("click", function () {
     //x변경
     menuBtn.classList.toggle("active");
+    //모바일 메뉴 나타남
     mobileMenu.classList.toggle("active");
 });
 // 화면 크기 변경시 모바일 메뉴 닫기
@@ -15,4 +16,15 @@ window.addEventListener("resize", function () {
         // 햄버거 버튼
         menuBtn.classList.remove("active");
     }
+});
+//모바일 메뉴 링크 클릭시
+const mbMenuLink = document.querySelectorAll(".mobile-menu a");
+//모바일메뉴 a 각각 꺼낸다
+mbMenuLink.forEach((link) => {
+    link.addEventListener("click", () => {
+        //모바일메뉴 닫기
+        mobileMenu.classList.remove("active");
+        //햄버거메뉴X -> 원래대로
+        menuBtn.classList.remove("active");
+    });
 });
